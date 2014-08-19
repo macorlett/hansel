7fab9b339f9e78db3b4a93cbfdd93d27cbdfdcd6
Michael <macorlett@gmail.com>
2014-08-19 22:21:02 +1200

    new test data
    added an image and text crumb to data for testing purposes.

--- js/test-data.js
+++ js/test-data.js
@@ -20,13 +20,17 @@ var trailData={
       "map":""
       },
       {
-      "type":"location",
-      "lon":-41.293008,
-      "lat":174.775097,
-      "zoom":16,
-      "threashhold":15,
-      "place":"62 Ghuznee street",
-      "map":""
+      "type":"image",
+      "sucess_type":"click",
+      "message":"In fermentum urna erat, eget tristique quam porttitor non. Aliquam mattis tellus libero, nec fringilla diam auctor ac volutpat.",
+      "location":[{"lat":"","lon":"","threshold":""}], //only used if success_type is location
+      "answer":"" //only used if success_type is answer
+      },
+      {
+      "type":"text",
+      "success_type":"answer", // can also be click
+      "message":"this has a 140 character limit",
+      "answer":"1234!", //only used if success_type is answer
       }
     ]
 }
